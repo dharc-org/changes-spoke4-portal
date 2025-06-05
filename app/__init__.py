698c899b1e72134b7d1dbb2ec09247ceb1dde1c1
giuliarenda <giuliarenda94@gmail.com>
2025-06-05 18:04:14 +0200

    fix: correct typo in extensions module and enable get_locale in templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from .extentions import babel, get_locale
+from .extensions import babel, get_locale
 from .routes import main
 
 
 def create_app():
     app = Flask(__name__)
-
+    app.jinja_env.globals['get_locale'] = get_locale
     app.config["BABEL_TRANSLATION_DIRECTORIES"] = "../translations"
 
     babel.init_app(app, locale_selector=get_locale)
