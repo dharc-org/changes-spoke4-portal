3afe5b5c783f314534952e51f27978f89a1fd22e
giuliarenda <giuliarenda94@gmail.com>
2026-04-27 12:45:27 +0200

    chore: make dev server host and port configurable

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Allow overriding port via PORT or FLASK_RUN_PORT env vars
+    # Default to 5050 to avoid conflicts with 5000 (Flask default) and 3030 (Fuseki)
+    port = int(os.environ.get('PORT')
+               or os.environ.get('FLASK_RUN_PORT') or 5050)
+    host = os.environ.get('HOST', '127.0.0.1')
+    app.run(debug=True, host=host, port=port)
